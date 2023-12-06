d626322fa4961d6d0c81b88024cacf13f59f5996
Tomer Amir <tomer@apiiro.com>
2023-12-06 11:47:21 +0200

    .

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
-password = "#@vfdsd435trGSAFg"
+password = "ghp_12345678901234567890123456789012"
 
-response = requests.get('', {'password': password})
+response = requests.get('http://google.com', {'password': password})
 
 print(response.json())
