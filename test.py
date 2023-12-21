3eb41f66aa6a08ac77a02592cf0195139093746f
Tomer Amir <tomer@apiiro.com>
2023-12-21 16:44:35 +0200

    .

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import requests
 
-password = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHQ"
+password1 = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHQ"
+password2 = "tomer123"
 
 response = requests.get('http://google.com', {'password': password})
 
