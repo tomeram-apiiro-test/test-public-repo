7a91a83f94636b784b1fc484a8fd27ceb4dc3a7c
Tomer Amir <tomer@apiiro.com>
2024-02-18 17:24:40 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import requests
 
 password1 = "test"
 password2 = "tomer123"
+password = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHQ"
 
 response = requests.get('http://google.com', {'password': password})
 
